772cc8e73651ee9e2769247442562dc6c5ba39a2
Deputatov Nikita <deputatovn@yandex.ru>
2019-06-11 13:18:42 +0300

    add pairs.js

--- js_data_pairs_on_strings/pairs.js
+++ js_data_pairs_on_strings/pairs.js
@@ -1,8 +1,3 @@
 const cons = (x, y) => `${x}\0${y}` 
 const car = (p) => p.slice(0, p.indexOf('\0'))
-const cdr = (p) => p.slice(p.indexOf('\0')++, p.length)
-
-const c = cons('one', 'two')
-
-// console.log(car(c))
-console.log(cdr(c))
+const cdr = (p) => p.slice(p.indexOf('\0') + 1, p.length)
